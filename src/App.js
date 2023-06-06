9559016d9cd88c18479858737e44ed0dfb63bd29
freddy24-7 <freddy.austli@aol.com>
2023-06-06 10:39:19 +0200

    final thunks logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import UsersList from "./components/UsersList";
+import UsersList from './components/UsersList';
 
-const App = () => {
-    return (
-        <div className="container mx-auto">
-            <UsersList />
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
+}
 
 export default App;
